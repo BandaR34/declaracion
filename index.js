dddacdad2cddf93db783cd630b1150e2ae31deb6
BandaR34 <96967218+BandaR34@users.noreply.github.com>
2022-01-01 22:45:43 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Te amo mi vida hermosa <3, Me haces muy feliz valeria')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
